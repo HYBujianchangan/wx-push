b34242ca3bc1a4f6613fabd46540ee02c25b33b0
HYBujianchangan <457863688@qq.com>
2024-04-18 11:19:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
-  touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
+  appid: "wxa9b397a57a1de810",
+  appsecret: "a11b358cbb0465ccc73aba6bf7fd0053",
+  template_id: "NUADNrtsHzOgk7Vx0Lrne-_rOMJLjduEia3y3QjPu0Y",
+  touser: "oHW4u6xS0pXIvb_Xh5KAwFC_AtoM",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
   city: "邢台",
